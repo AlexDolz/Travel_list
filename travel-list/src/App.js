1ef8a23d78b68dd32508b056ebbc82f7cbd99d87
AlexDolz <aleksejdolznikov3@gmail.com>
2024-09-11 17:29:06 +0200

    started

---
+++ travel-list/src/App.js
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far away 👜</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
