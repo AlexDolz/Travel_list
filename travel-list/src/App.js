ae96a62c22b5cf2e3d7946b3d06881a5d00e8ff9
AlexDolz <aleksejdolznikov3@gmail.com>
2024-09-28 14:28:36 +0200

    derived state

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToogleItems={handleToogleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -108,10 +108,26 @@ function Item({ item, onDeleteItem, onToogleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈"
+          : `💼 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
